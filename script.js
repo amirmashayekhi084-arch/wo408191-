03931f9a4e348b8471b78b9fa994aa2421ffb46c
amir mashayekhi <amirms88@outlook.com>
2025-11-10 21:00:48 +0330

    wo408191 project

---
+++ script.js
@@ -0,0 +1,5 @@
+const word = ("hello world !")
+const word2 = ("open the door , please")
+// here is word 2 for the first paragraph and word2 for the second
+console.log(word2.split("").reverse().join("") )
+console.log(word.split("").reverse().join(""))
